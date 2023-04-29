9c9c2e5db295a2c3095f35337ec41afc7ce8977d
zak freifer <freifery@gmail.com>
2023-04-29 15:50:28 +0100

    crud app done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  post '/tasks', to: 'tasks#create'
+
+   # route to display the edit form
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+   # route to handle the submission of the edited task
+   patch '/tasks/:id', to: 'tasks#update'
+   delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+
+
+
 end
